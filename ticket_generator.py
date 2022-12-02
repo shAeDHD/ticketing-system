edc4a44daf11ea12736dc2975a01c86407f2965f
Shae Riches <shaeriches@gmail.com>
2022-12-02 23:59:12 +1100

    ticket_generator.py working successfully

--- ticket_generator.py
+++ ticket_generator.py
@@ -3,11 +3,12 @@ from random_address import real_random_address
 import datetime
 from datetime import timedelta
 import random 
+import json
 
-tickets = []
+tickets = [ ]
 
 
-def generate_tickets( number_of_tickets ): #NOTE: need to use self?
+def generate_tickets( number_of_tickets ): 
     
     ticket_ids = [704]
     note_ids = [4025864]
@@ -16,7 +17,7 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
         new_ticket_id = ticket_ids[-1] + 1
         ticket_ids.append(new_ticket_id)
         return new_ticket_id
-    ticket_id_value = generate_ticket_id
+    ticket_id_value = generate_ticket_id()
     
     def generate_performer_id():
         performer_id = random.randint(149900,150000)
@@ -32,19 +33,6 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
     end_date    = start_date + timedelta(days=random.randint(1,3))
     
     def generate_date( start_date, end_date ): 
-    
-    # time_between_dates = end_date - start_date
-    # print(time_between_dates)     #   check == true
-    
-    # days_between_dates = time_between_dates.days
-    # print(days_between_dates)     #   check == true
-    
-    # random_number_of_days = random.uniform(0.25, days_between_dates)
-    # print(random_number_of_days)  #   check == true
-    
-    # performed_at = datetime.timedelta(days=random_number_of_days)
-    # print(performed_at)           #   check == true
-    
         performed_at = start_date + timedelta(days=random.uniform(0.25 , (end_date - start_date).days +1))
         return performed_at
     activity_commencement_date = generate_date( start_date, end_date )
@@ -66,16 +54,13 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
         return boolean_value         
     contacted_customer_value = generate_boolean()
     
-    def generate_random_activity( shipment_date_value ):
-        
-        # Example activities/ticket topics
+    def generate_random_activity( shipment_date_value ):    
+    # Example activities/ticket topics
         activities = [ "Delivery Issue", "Refund Request", "Other / Message" ]
         activity_type = activities[random.randint( 0, 2 )] #NOTE for testing, change index value
-        
         # Example issue types and groupings
         issue_type = [ "Incident", "Faulty", "Damaged on Arrival", "Warranty"] 
         group = [ "Refund", "Replacement", "Reparation" ]
-        
         # Status values given from docs and random selection
         status_options = [ 
                 "Open",
@@ -86,20 +71,18 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
                 "Pending"  
                 ]
         status_value = status_options[ random.randint( 0, (len(status_options)-1) ) ]
-        
-        # Example Product Lines 
+        # Example Categories and iteration selection        
+        category = [ "phone", "laptop", "pc", "other" ] 
+        category_value = category[random.randint( 0, len(category) -1 )]
+        # Example Product Lines and iteration selection
         products = {
             "phone" : [ "Mobile", "Landline" ],
             "laptop" : [ "Mac", "Acer", "Lenovo" ],
             "pc" : [ "Alienware", "Dell", "Custom" ],
             "other" : [ "Accessories", "Office Utilities" ]
         }
-        category = [ "Phone", "Laptop", "PC", "Other" ] 
-        
-        # Random iteration product selection        
-        product_category = category[random.randint( 0, len(category) -1 )]
-        
-        # Random generation of iterations activity type
+        product_value = products[category_value][random.randint( 0,( len( products[category_value] ) -1 ) )]
+        # Generation of iterations activity type
         if activity_type == "Other / Message":
             return {
                 "note" : {
@@ -109,9 +92,9 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
             }
         else:
             return {
-                "shipping_address": real_random_address(),
-                "shipment_date": shipment_date_value,
-                "category" : product_category,
+                "shipping_address": f'{real_random_address()}',
+                "shipment_date": f'{shipment_date_value}',
+                "category" : category_value,
                 "contacted_customer" : contacted_customer_value,
                 "issue_type": issue_type[random.randint( 0, 3 )],
                 "source" : random.randint( 1, 20 ),
@@ -120,33 +103,39 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
                 "group" : group[random.randint( 0, len(group) -1 )],
                 "agent_id" : performer_id_value,
                 "requester" : requester_value,
-                "product" : products([product_category],[random.randint( 0, (len(product_category) -1) )])
-            }
+                "product" : product_value
+            }    
+    activity_value = generate_random_activity( activity_commencement_date )
     
     iteration = 1
     while iteration <= number_of_tickets:
-
         
         if iteration % 50 == 0: 
             activities_data = {
-                "performed_at": activity_commencement_date,
-                "ticket_id": ticket_id_value,
+                "performed_at": f'{activity_commencement_date}',
+                "ticket_id": f'{ticket_id_value}',
                 "performer_type" : 'Admin',
                 "performer_id" : 149899,
-                "activity" : generate_random_activity( activity_commencement_date )
+                "activity" : f'{activity_value}'
             }
             iteration = iteration + 1
-            return activities_data
+            tickets.append(activities_data)
         
         else:
             activities_data = {
-                "performed_at": activity_commencement_date,
-                "ticket_id": ticket_id_value,
+                "performed_at": f'{activity_commencement_date}',
+                "ticket_id": f'{ticket_id_value}',
                 "performer_type" : 'User',
                 "performer_id" : performer_id_value,
-                "activity" : generate_random_activity( activity_commencement_date )
+                "activity" : activity_value
             }
             iteration = iteration + 1
-            return activities_data              
-        
-print(tickets.append( generate_tickets( 2 ) ))
+            
+            tickets.append(activities_data)
+
+
+# NOTE -> To change the amount of tickets generated, change
+#        the integer argument of generate_tickets() on line 137.
+tickets.insert( 0, generate_tickets( 1000 ) ) 
+
+print(json.dumps(tickets, sort_keys=True, indent=4) )     #   check == true
