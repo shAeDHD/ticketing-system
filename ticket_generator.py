6e1df423faddd81bf71b4329668422c2f6f7f122
Shae Riches <shaeriches@gmail.com>
2022-12-02 22:00:39 +1100

    begin trouble shooting

--- ticket_generator.py
+++ ticket_generator.py
@@ -4,98 +4,32 @@ import datetime
 from datetime import timedelta
 import random 
 
-######################## TEST ##############################
-start_date  = datetime.datetime.now()
-end_date    = start_date + timedelta(days=random.randint(1,3))
- 
-# def activity_commenced_at( start_date, end_date ): 
-    
-    # time_between_dates = end_date - start_date
-    # print(time_between_dates)     #   check == true
-    
-    # days_between_dates = time_between_dates.days
-    # print(days_between_dates)     #   check == true
-    
-    # random_number_of_days = random.uniform(0.25, days_between_dates)
-    # print(random_number_of_days)  #   check == true
-    
-    # performed_at = datetime.timedelta(days=random_number_of_days)
-    # print(performed_at)           #   check == true
-    
-    # performed_at = start_date + timedelta(days=random.uniform(0.25 , (end_date - start_date).days +1))
-    
-    # return performed_at
-        
-print(start_date)
-print(activity_commenced_at(start_date, end_date))
-print(end_date)
-
-
-
-
-        
-
-
-
-
-#   activities_data: [
-#       {
-#           "performed_at"      = performed_at(start_date, end_date)
-#           "ticket_id"         = generate_ticket_id()
-#           "performer_type"    = "user"        # maybe user/admin?
-#           "performer_id"      = generate_user_id())  
-#           "activity"          = {
-#               "note"          = {
-#                   "id"        = generate_note_id()
-#                   "type"      = random.randomint( 1, 4 )
-#               }
-#           }         
-
-
-
-
-#           "activity"          = {    
-#               "shipping_address"      = N/A or real_random_address()
-#               "shipment_date"         = 
-#               "category"              = phone, laptop, desktop,
-#               "contacted_customer"    = boolean
-#               "issue_type"            = incident, delivery,
-#               "source"                = random.randomInt()
-#               "status"                = open, close, pending, Resolved, Waiting for Customer, Waiting for Third Party
-#               "priority"              = randomInt(1 , 4)
-#               "group"                 = refund, replacement, reparation
-#               "agent_id"              = random.randomInt(149000, 150000)
-#               "requester"             = random.randomInt(149000, 150000)
-#               "product"               = mobile,    
-#           }
-#       }
-
-
-###########################################
-# DETAILS #
-
-# Metadata is time period being queried for, and number of tickets during that period 
-# Activities_data, then is a list of all tickets 
-
 tickets = []
-    
+
+
 def generate_tickets( number_of_tickets ): #NOTE: need to use self?
     
-    
-    performer_type = [ "User", "Admin" ]
-    phone = [ "Mobile", "Landline" ]
-    laptop = [ "Mac", "Acer", "Lenovo" ]
-    pc = [ "Alienware", "Dell", "Custom" ]
-    other = [ "Accessories", "Office Utilities" ]
-    
-    category = [ 'Phone', 'Laptop', 'PC', 'Other' ] 
-    issue_type = [ 'Incident', 'Delivery' ]  
-    status = [ 'Open', 'Close', 'Pending', 'Resolved' ]
-    group = [ 'refund', 'replacement', 'reparation' ]
-    
-    performer_ids = []
     ticket_ids = [704]
     note_ids = [4025864]
+    
+    def generate_ticket_id():
+        new_ticket_id = ticket_ids[-1] + 1
+        ticket_ids.append(new_ticket_id)
+        return new_ticket_id
+    ticket_id_value = generate_ticket_id
+    
+    def generate_performer_id():
+        performer_id = random.randint(149900,150000)
+        return performer_id
+    performer_id_value = generate_performer_id()
+    
+    def generate_requester_data():
+        requester = random.randint(149900,149998)
+        return requester
+    requester_value = generate_requester_data()
+
+    start_date  = datetime.datetime.now()
+    end_date    = start_date + timedelta(days=random.randint(1,3))
     
     def generate_date( start_date, end_date ): 
     
@@ -112,56 +46,87 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
     # print(performed_at)           #   check == true
     
         performed_at = start_date + timedelta(days=random.uniform(0.25 , (end_date - start_date).days +1))
-    
         return performed_at
     activity_commencement_date = generate_date( start_date, end_date )
-   
-    def generate_ticket_id():
-        new_ticket_id = ticket_ids[-1] + 1
-        ticket_ids.append(new_ticket_id)
-        return new_ticket_id
-    ticket_id_value = generate_ticket_id
-    
-    def generate_performer_id():
-        performer_id = random.randomint(149900,150000)
-        return performer_id
-    performer_id_value = generate_performer_id()
 
     def generate_shipment_date( iteration_generated_date ):
         date_before_complaint = iteration_generated_date - timedelta(days=random.randint( 3, 10 ))
         return date_before_complaint
     shipment_date_value = generate_shipment_date( activity_commencement_date )
-        
+
     def generate_note_id():
         new_note_id = note_ids[-1] + 1
         note_ids.append(new_note_id)
         return new_note_id
+    note_id_value = generate_note_id()
     
     def generate_boolean():
         boolean_value = False
         if random.randint(0, 1) == 0: boolean_value = True
         return boolean_value         
     contacted_customer_value = generate_boolean()
-
-
+    
     def generate_random_activity( shipment_date_value ):
-        activities = [ "Delivery Issue", "Refund Request", "Other / Message" ]
-        activity_type = activities[random.randint( 0, 2 )]
         
-        if activity_type == "Delivery Issue":
+        # Example activities/ticket topics
+        activities = [ "Delivery Issue", "Refund Request", "Other / Message" ]
+        activity_type = activities[random.randint( 0, 2 )] #NOTE for testing, change index value
+        
+        # Example issue types and groupings
+        issue_type = [ "Incident", "Faulty", "Damaged on Arrival", "Warranty"] 
+        group = [ "Refund", "Replacement", "Reparation" ]
+        
+        # Status values given from docs and random selection
+        status_options = [ 
+                "Open",
+                "Close",
+                "Resolved",
+                "Waiting for Customer",
+                "Waiting for Third Party",
+                "Pending"  
+                ]
+        status_value = status_options[ random.randint( 0, (len(status_options)-1) ) ]
+        
+        # Example Product Lines 
+        products = {
+            "phone" : [ "Mobile", "Landline" ],
+            "laptop" : [ "Mac", "Acer", "Lenovo" ],
+            "pc" : [ "Alienware", "Dell", "Custom" ],
+            "other" : [ "Accessories", "Office Utilities" ]
+        }
+        category = [ "Phone", "Laptop", "PC", "Other" ] 
+        
+        # Random iteration product selection        
+        product_category = category[random.randint( 0, len(category) -1 )]
+        
+        # Random generation of iterations activity type
+        if activity_type == "Other / Message":
+            return {
+                "note" : {
+                    "id" : note_id_value,
+                    "type" : group[random.randint( 0, len(group) -1 )]
+                }
+            }
+        else:
             return {
                 "shipping_address": real_random_address(),
                 "shipment_date": shipment_date_value,
-                "category" : "Delivery Issue",
-                "contacted_customer" : contacted_customer_value
+                "category" : product_category,
+                "contacted_customer" : contacted_customer_value,
+                "issue_type": issue_type[random.randint( 0, 3 )],
+                "source" : random.randint( 1, 20 ),
+                "status" : status_value,
+                "priority" : random.randint( 1, 5),
+                "group" : group[random.randint( 0, len(group) -1 )],
+                "agent_id" : performer_id_value,
+                "requester" : requester_value,
+                "product" : products([product_category],[random.randint( 0, (len(product_category) -1) )])
             }
-        
+    
+    iteration = 1
+    while iteration <= number_of_tickets:
 
         
-            
-    iteration = 1
-    
-    while iteration <= len(number_of_tickets):
         if iteration % 50 == 0: 
             activities_data = {
                 "performed_at": activity_commencement_date,
@@ -170,6 +135,9 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
                 "performer_id" : 149899,
                 "activity" : generate_random_activity( activity_commencement_date )
             }
+            iteration = iteration + 1
+            return activities_data
+        
         else:
             activities_data = {
                 "performed_at": activity_commencement_date,
@@ -177,5 +145,8 @@ def generate_tickets( number_of_tickets ): #NOTE: need to use self?
                 "performer_type" : 'User',
                 "performer_id" : performer_id_value,
                 "activity" : generate_random_activity( activity_commencement_date )
-                
             }
+            iteration = iteration + 1
+            return activities_data              
+        
+print(tickets.append( generate_tickets( 2 ) ))
