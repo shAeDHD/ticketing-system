5bf3146555f6d71e0e76f9e1b2c6cd1bd21cbcc6
Shae Riches <shaeriches@gmail.com>
2022-12-03 15:55:23 +1100

    further refactor

--- ticket_generator.py
+++ ticket_generator.py
@@ -1,47 +1,36 @@
 from random_address import real_random_address
 import datetime
-from datetime import timedelta
+from datetime import timezone, timedelta
 import random 
 import json
 
-tickets = []
-requested_number = input()
+requested_number = input("Input required ticket amount: ")
+
+tickets     = []
+end_date    = datetime.datetime.now( timezone.utc ).replace(microsecond=0)
+start_date  = end_date - timedelta( days=random.randint( 1,3 ) )
+shipment_date_value = end_date - timedelta( days=random.randint(3, 10), hours=random.randint(5, 10) )
+ticket_ids = [704]
+note_ids = [4025864]
+performer_id_value = random.randint(149900,150000)   
+
+def generate_performer_type( number_of_tickets ):
+    if len(number_of_tickets) % 50 == 1:
+        performer_type_value = "Admin"
+        return performer_type_value
+    else:
+        performer_type_value = "User"
+        return performer_type_value
 
 def generate_tickets( number_of_tickets ): 
 
-    iteration = 1
-    
-    while iteration <= number_of_tickets:
-        
-        ticket_ids = [704]
-        note_ids = [4025864]
-        start_date  = datetime.datetime.now()
-        end_date    = start_date + timedelta(days=random.randint(1,3))
-        
-        performer_id_value = random.randint(149900,150000)
-        
-        def generate_date( start_date, end_date ): 
-            performed_at = start_date + timedelta(days=random.uniform(0.25 , (end_date - start_date).days +1))
-            return performed_at
-        activity_commencement_date = generate_date( start_date, end_date )
-        
-        def generate_performer_type( number_of_tickets ):
-            if len(number_of_tickets) % 50 == 1:
-                performer_type_value = "Admin"
-                return performer_type_value
-            else:
-                performer_type_value = "User"
-                return performer_type_value
-            
-        def generate_random_activity( ):    
-    
-            # Example activities/ticket topic
+    for iteration in range(number_of_tickets):
+
+        def generate_activity():    
+            # Examples for random data generation
             activities = [ "Delivery Issue", "Refund Request", "Other / Message" ]
-            activity_type = random.choice(activities)
-            # Example issue types and groupings
             issue_type = [ "Incident", "Faulty", "Damaged on Arrival", "Warranty"] 
             group = [ "Refund", "Replacement", "Reparation" ]
-            # Status values given from docs and random selection
             status_options = [ 
                 "Open",
                 "Close",
@@ -49,11 +38,9 @@ def generate_tickets( number_of_tickets ):
                 "Waiting for Customer",
                 "Waiting for Third Party",
                 "Pending"  
-            ]
-            # Example Categories and iteration selection        
+            ]    
             category = [ "phone", "laptop", "pc", "other" ] 
             category_value = random.choice(category)
-            # Example Product Lines and iteration selection
             products = {
                 "phone" : [ "Mobile", "Landline" ],
                 "laptop" : [ "Mac", "Acer", "Lenovo" ],
@@ -61,8 +48,8 @@ def generate_tickets( number_of_tickets ):
                 "other" : [ "Accessories", "Office Utilities" ]
             }
             
-            # Generation of iterations activity type
-            if activity_type == "Other / Message":
+            # Generation of iterations activity
+            if random.choice(activities) == "Other / Message":
                 return {
                     "note" : {
                         "id" : note_ids[-1] + 1,
@@ -72,7 +59,7 @@ def generate_tickets( number_of_tickets ):
             else:
                 return {
                     "shipping_address": real_random_address(),
-                    "shipment_date": f'{activity_commencement_date - timedelta(days=random.randint( 3, 10 ))}',
+                    "shipment_date": f'{shipment_date_value.strftime( "%d-%m-%Y %H:%M:%S %z")}',
                     "category" : category_value,
                     "contacted_customer" : random.choice([True, False]),
                     "issue_type": random.choice(issue_type),
@@ -86,11 +73,11 @@ def generate_tickets( number_of_tickets ):
                 }
                          
         activities_data = {
-            "performed_at": f'{activity_commencement_date}',
+            "performed_at": f'{end_date.strftime( "%d-%m-%Y %H:%M:%S %z")}',
             "ticket_id": ticket_ids[-1] + 1,
             "performer_type" : generate_performer_type(ticket_ids),
             "performer_id" : performer_id_value,
-            "activity" : generate_random_activity( )
+            "activity" : generate_activity( )
         }
         
         tickets.append(activities_data)
