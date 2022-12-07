f905777707ae82a90d29b8367702955caa48e221
Shae Riches <shaeriches@gmail.com>
2022-12-07 14:21:05 +1100

    removed generator .git

--- ticket_database/db/seeds.rb
+++ ticket_database/db/seeds.rb
@@ -4,7 +4,6 @@
 #     Ticket.create!(json_ticket)
 # end
 
-
 generated_tickets = File.read("C:/Users/shaeriches/coding-stuff/tech-challenges/aginic/ticket_generator/json_ticket_data.json")
 
 JSON.parse( generated_tickets ).each do | json_ticket |
