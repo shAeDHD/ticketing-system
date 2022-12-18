ae2057682523a240fa2a1ce369fc03034d3507f0
Shae Riches <shaeriches@gmail.com>
2022-12-18 23:14:37 +1100

    remade create_tables.sql

--- ticket_database/create_tables.sql
+++ ticket_database/create_tables.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS metadata;
-DROP TABLE IF EXISTS ticket_headers;
+DROP TABLE IF EXISTS tickets;
 DROP TABLE IF EXISTS note_activities;
 DROP TABLE IF EXISTS other_activities;
 
@@ -11,21 +11,27 @@ CREATE TABLE metadata (
     activities_count INTEGER
 );
 
-CREATE TABLE ticket_headers (
+CREATE TABLE tickets (
+    id INTEGER PRIMARY KEY,
+    ticket_id INTEGER,
+    activities_assigned INTEGER
+)
+
+
+CREATE TABLE note_ticket (
     id INTEGER PRIMARY KEY, 
     performed_at TEXT,
-    ticket_id INTEGER,
     performer_type TEXT,
-    performer_id INTEGER
-);
-
-CREATE TABLE note_activities (
-    id INTEGER PRIMARY KEY,
+    performer_id INTEGER,
+    note_id INTEGER
     note_type INTEGER
 );
 
-CREATE TABLE other_activities (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE other_ticket (
+    id INTEGER PRIMARY KEY, 
+    performed_at TEXT,
+    performer_type TEXT,
+    performer_id INTEGER,
     shipping_address TEXT,
     shipment_date TEXT,
     category TEXT,
--- ticket_database/server.rb
+++ ticket_database/server.rb
@@ -12,35 +12,25 @@ class Metadata < ActiveRecord::Base
     has_many :other_activities, through: :ticket_header
     has_one :note_activity, through: :ticket_header
     def create
-        # Where multiple activities share the same performer id,
-        # create a Metadata.new. That has a start_at 
-        # which is set as before the earliest 
-        # ticket.performed_at and ends at time.now.
-        # the activites_count is activites.length.
-        # metadata = Metadata.new
-        # metadata.activites_count =
         puts "made to metadata new" 
     end
 
     def start_time(  )
-        # parse string time into date time
-
-        # take random number of hours/days from this date
     end
 end
-class Ticket_Header < ActiveRecord::Base
+class Ticket < ActiveRecord::Base
     has_many :other_activities
     belongs_to :metadata
     def create
     end
 end
-class Note_Activity < ActiveRecord::Base
+class Note_Ticket < ActiveRecord::Base
     belongs_to :ticket_header
     # belongs_to :metadata, through: :ticket_header
     def create 
     end
 end
-class Other_Activity < ActiveRecord::Base
+class Other_Ticket < ActiveRecord::Base
     belongs_to :ticket_header
     # belongs_to :metadata, through: :ticket_header
     def create
@@ -55,33 +45,21 @@ puts ActiveRecord::Base.connection.tables
 
 ######      MAPS JSON DATA INTO DB TABLES       ######
 ticket_data.map do | ticket_iteration |
-    
-    iteration_th = Ticket_Header.new
-    iteration_th.performed_at = ticket_iteration.dig("performed_at"),
-    iteration_th.ticket_id = ticket_iteration.dig("ticket_id"),
-    iteration_th.performer_type = ticket_iteration.dig("performer_type")
-    iteration_th.performer_id = ticket_iteration.dig("performer_id")
-    iteration_th.save
 
-    activity = ticket_iteration.fetch("activity")
-
-    if activity.has_key?("note")
+    if ticket_iteration.has_key?("note")
         #   Put Note data into database
-        note = Note_Activity.new 
+        note = Note.new 
+        note.performed_at = ticket_iteration.dig("performed_at"),
+        note.performer_type = ticket_iteration.dig("performer_type")
+        note.performer_id = ticket_iteration.dig("performer_id")
         note.id = activity.dig( "note", "id" )
         note.note_type = activity.dig( "note", "type" )
         note.save
-        #   For each note, we want to create a Metadata record as 
-        #   each chain of tickets can only have one note. To this, 
-        #   we will tie the 'performer_id' for easy identification.
-        metadatum = Metadata.new
-        metadatum.performer_id = ticket_iteration.dig("performer_id")
-        # metadatum.start_at = Metadata.start_time
-        puts ticket_iteration.fetch("performed_at")
-        metadatum.save
-
     else
-        other = Other_Activity.new 
+        other = Other.new 
+        other.performed_at = ticket_iteration.dig("performed_at"),
+        other.performer_type = ticket_iteration.dig("performer_type")
+        other.performer_id = ticket_iteration.dig("performer_id")
         other.shipping_address = activity.dig( "shipping_address" )
         other.shipment_date = activity.dig( "shipment_date" )
         other.category = activity.dig( "category" )
@@ -95,7 +73,6 @@ ticket_data.map do | ticket_iteration |
         other.requester = activity.dig( "requester" )
         other.product = activity.dig( "product" )
         other.save
-        # if other.status == "Resolved" || other.status == "Closed"
 
     end #   end else/if
 end     #   end JSON data mapping
